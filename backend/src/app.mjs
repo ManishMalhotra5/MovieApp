7aa278a757e57b9bf719b421cd180273525bbc6d
Manish Malhotra <manish21303105@gmail.com>
2024-08-27 22:20:18 +0530

    urlencoded

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -1,19 +1,20 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import userRouter from "./routes/user.route.mjs";
-import movieRouter from "./routes/movie.route.mjs"
 
 
 const app = express();
-
-app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
 app.use(express.json({limit:"16kb"}));
+app.use(cookieParser());
 app.use(cors());
+
+import userRouter from "./routes/user.route.mjs";
+import movieRouter from "./routes/movie.route.mjs";
 
 app.use("/api/user",userRouter);
 app.use("/api/movie",movieRouter);
 
-import "./config/createSuperAdmin.mjs";
+
 
 export default app;
